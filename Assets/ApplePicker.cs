e79b508e6c78346cf9a3048e0dd3d989b1ecc3e2
ConnorWall <connorwall@u.boisestate.edu>
2018-04-21 13:17:23 -0600

    Added apple picker
    Adding scrolling background to apple picker game

---
+++ Assets/ApplePicker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public GameObject basketPrefab;
+	public int numBaskets = 3;
+	public float basketBottom = -14f;
+	public float basketSpace = 2f;
+    public List<GameObject> basketList;
+    public GameObject tBasketGO;
+
+	// Use this for initialization
+	void Start () {
+        basketList = new List<GameObject>();
+
+		for (int i = 0; i < numBaskets; i++) {
+			GameObject basket = Instantiate<GameObject> (basketPrefab);
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottom + (basketSpace * i);
+			basket.transform.position = pos;
+            basketList.Add(tBasketGO);
+		}
+	}
+
+    //Declared public to be seen by other classes
+    public void AppleDestroyed()
+    {
+        //Destroy all of the falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+        //Get index of the last basket in basketList
+        int basketIndex = basketList.Count - 1;
+        //Get a reference to that Basket GameObject
+        tBasketGO = basketList[basketIndex];
+        //Remove the basket from list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        //If there are no baskets left, restart the game
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scene_0");
+        }
+    }
+}
