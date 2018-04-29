fb3232f68388f3dd0d8b74d3ce38a76b378b6356
MyGriever <35589353+MyGriever@users.noreply.github.com>
2018-04-29 01:31:40 -0600

    Updated

---
+++ ApplePickerGameUpdated429/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+
+   /* GAMEOVER
+    
+    private float endGameCounter;
+
+    void FixedUpdate()
+    {
+        endGameCounter++;
+        if (endGameCounter % enter number of seconds here == 0)
+        {
+            do stuff
+        }
+    }
+    */
+
+    //Declared public to be seen by other classes
+    public void StarDestroyed()
+    {
+        //Destroy all of the falling apples
+        GameObject[] starArray = GameObject.FindGameObjectsWithTag("Star");
+        foreach (GameObject tGO in starArray)
+        {
+            Destroy(tGO);
+        }
+    }
+}
